363273e2515f42700cde15b6001c26de7d8fe7a9
LeandroCarvalho21 <leandrocnovais@gmail.com>
2025-11-18 00:15:53 -0300

    Adicionando script para fazer interação com header

---
+++ js/script.js
@@ -0,0 +1,16 @@
+let ultimoScroll = 0;
+const header = document.getElementById("header");
+
+window.addEventListener("scroll", function () {
+    let scrollAtual = window.scrollY;
+
+    if (scrollAtual < ultimoScroll) {
+        // rolando para cima → mostrar
+        header.classList.add("mostrar");
+    } else {
+        // rolando para baixo → esconder
+        header.classList.remove("mostrar");
+    }
+
+    ultimoScroll = scrollAtual;
+});
